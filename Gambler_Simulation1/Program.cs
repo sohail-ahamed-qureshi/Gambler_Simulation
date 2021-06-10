ed73a85a60dda3de1a486fe7fb5a83f738e6c730
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-10 12:33:24 +0530

    [sohail] Add: gambler bets for days

---
+++ Gambler_Simulation1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Gambler_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Simulation");
+
+            //variable
+            int gamblerStake = 100;
+         
+            //constants
+            const int BET =1;
+             int days = 30;
+            
+            while (days != 0)
+            {
+                gamblerStake -= BET;
+                days--;
+                Console.WriteLine(gamblerStake);
+            }
+        }
+    }
+}
