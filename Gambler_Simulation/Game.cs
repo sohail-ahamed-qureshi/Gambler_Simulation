f610873c75c60648fb940415ca1ee58a1e7807a6
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-10 13:38:23 +0530

    [sohail] Add: Gambler bets 1$

---
+++ Gambler_Simulation/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler_Simulation
+{
+    class Game
+    {
+        public void Gambler()
+        {
+            const int WIN = 0;
+            const int LOSE = 1;
+            Random random = new Random();
+            int game = random.Next(0, 2);
+            if (game == WIN)
+            {
+                Console.WriteLine("gambler wins!");
+            }
+            if (game == LOSE)
+            {
+                Console.WriteLine("gambler lose!");
+            }
+
+        }
+    }
+}
--- Gambler_Simulation/Program.cs
+++ Gambler_Simulation/Program.cs
@@ -11,24 +11,10 @@ namespace Gambler_Simulation
             //constants
             const int STAKE = 100;
             const int BET =1;
-            Gambler();
+            Game game = new Game();
+            game.Gambler();
         }
 
-        public static void Gambler()
-        {
-            const int WIN = 0;
-            const int LOSE = 1;
-            Random random = new Random();
-            int game = random.Next(0,2);
-            if (game == WIN)
-            {
-                Console.WriteLine("gambler wins!");
-            }
-            if (game == LOSE)
-            {
-                Console.WriteLine("gambler lose!");
-            }
-
-        }
+       
     }
 }
